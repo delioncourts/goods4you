50f465eec6c1366c3e003bf871e1c99d7e25a276
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2024-02-13 15:21:07 +0300

    feat: upd readme, catalog adaptive

--- src/widgets/team/Team.tsx
+++ src/widgets/team/Team.tsx
@@ -13,27 +13,26 @@ const Team = () => {
     return (
         <section className={styles.team}>
             <div className={styles.container}>
-            <h2 className={styles.heading}>Our team</h2>
+                <h2 className={styles.heading}>Our team</h2>
 
-         <div className={styles.team__list}>
-            <div className={styles.team__column}>
-         <TeamCard name="Alex" title="Manager" image={team_mate_1}/>
-         <TeamCard name="Alex" title="Manager" image={team_mate_2} />
-         </div>
+                <div className={styles.team__list}>
+                    <div className={styles.team__column}>
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_1} />
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_2} />
+                    </div>
 
-         <div  className={styles.team__column_up}>
-         <TeamCard name="Alex" title="Manager" image={team_mate_3}/>
-         <TeamCard name="Alex" title="Manager" image={team_mate_4}/>
-         </div>
+                    <div className={styles.team__column_up}>
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_3} />
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_4} />
+                    </div>
 
-         <div  className={styles.team__column}>
-         <TeamCard name="Alex" title="Manager" image={team_mate_5}/>
-         <TeamCard name="Alex" title="Manager" image={team_mate_6}/>
+                    <div className={styles.team__column}>
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_5} />
+                        <TeamCard name="Maxim" title="Administrator" image={team_mate_6} />
+                    </div>
+                </div>
 
-         </div>
-         </div>
 
-  
             </div>
         </section>
     )
